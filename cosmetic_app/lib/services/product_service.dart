5be81716fed386b8b59c01fc47c7f55680242428
Abishani Muthalagan <66344633+Abishani@users.noreply.github.com>
2024-09-24 02:21:19 +0530

    chnages in product services file

--- cosmetic_app/lib/services/product_service.dart
+++ cosmetic_app/lib/services/product_service.dart
@@ -1,13 +1,17 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../models/product.dart';
 
 class ProductService {
-  Future<List<dynamic>> fetchProducts() async {
-    final response =
-        await http.get(Uri.parse('https://dummyjson.com/products'));
+  static const String url = 'https://dummyjson.com/products';
 
+  Future<List<Product>> fetchProducts() async {
+    final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
-      return json.decode(response.body)['products'];
+      var jsonResponse = json.decode(response.body);
+      print(jsonResponse); // Add this line to print the JSON response
+      List productsJson = jsonResponse['products'];
+      return productsJson.map((product) => Product.fromJson(product)).toList();
     } else {
       throw Exception('Failed to load products');
     }
